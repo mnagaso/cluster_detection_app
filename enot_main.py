fb741b5ba8098a1cc0c8682b500cea4659ae99ee
Masaru Nagaso <mnsaru22@gmail.com>
2016-10-21 16:06:55 +0200

    added main script and lib directory for original modules

---
+++ enot_main.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+'''
+
+here for some discrition about this codes
+
+'''
+
+
+# inport modules
+
+
+if __name__ == '__main__':
+# call function by executing order
+
+# read input first
+
+# initialize
+## calculate p_alpha
+## 
+
+# search algorithm for hierarchical mapping starts from here
+
+# output 
+
+# visualize
